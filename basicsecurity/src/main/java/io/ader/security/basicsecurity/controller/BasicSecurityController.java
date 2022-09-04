0bd914c926fd18ab8c678736b234d95519433c31
kakaobnk1 <kakaobnk1@kakaobnk1ui-MacBookPro.local>
2022-09-04 17:09:04 +0900

    basic security project upload

---
+++ basicsecurity/src/main/java/io/ader/security/basicsecurity/controller/BasicSecurityController.java
@@ -0,0 +1,12 @@
+package io.ader.security.basicsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicSecurityController {
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}
